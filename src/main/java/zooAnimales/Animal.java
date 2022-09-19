43069345084ebde35cb6cebd863fd154ec073c0c
StepXo <66577156+StepXo@users.noreply.github.com>
2022-09-19 04:14:40 -0500

    ey pero que paso aqui

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -25,11 +25,11 @@ public class Animal {
     public String movimiento(){return "desplazarse";}
 
     public static String totalPorTipo() {
-        return "Mamiferos: " + Mamifero.cantidadMamiferos() +
-                "\nAves: " + Ave.cantidadAves() +
-                "\nReptiles: " + Reptil.cantidadReptiles() +
-                "\nPeces: " + Pez.cantidadPeces() +
-                "\nAnfibios: " + Anfibio.cantidadAnfibios();
+        return "Mamiferos: " + Mamifero.cantidadMamiferos() + "\n" +
+                "Aves: " + Ave.cantidadAves() + "\n"+
+                "Reptiles: " + Reptil.cantidadReptiles() + "\n"+
+                "Peces: " + Pez.cantidadPeces() + "\n"+
+                "Anfibios: " + Anfibio.cantidadAnfibios();
     }
 
     public String toString() {
